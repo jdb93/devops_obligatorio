6b82d78ce90c7b7899d6e5a51e6cb9aeaa63e5ff
jdb93 <jdbriozzo@hotmail.com>
2025-11-16 12:26:23 -0300

    add infra folders and vpc/variables.tf file

---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,26 @@
+variable "vpc_cidr_block" {
+    description = "CIDR block for the VPC."
+    type        = string
+}
+
+variable "public_subnet_cidrs" {
+    description = "List of CIDR blocks for public subnets."
+    type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets."
+  type        = list(string)
+}
+
+variable "availability_zones" {
+    description = "List of availability zones to use for the subnets."
+    type        = list(string)
+}
+
+
+variable "tags" {
+  description = "Tags to apply to networking resources."
+  type        = map(string)
+  default     = {}
+}
