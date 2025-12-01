8e7c00eeb83d4dda00af9d8667f5a34ebebc11a9
jdb93 <jdbriozzo@hotmail.com>
2025-11-30 22:50:47 -0300

    fix file de backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "180358-stockwiz-backend"
-    key    = "staging/terraform.tfstate" #pasar a "default/terraform.tfstate" 
+    key    = "default/terraform.tfstate"
     region = "us-east-1"
   }
 }
